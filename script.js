10ed38ad11500213188de82c75283910d2c53819
wichigo <wichigofr@gmail.com>
2021-08-25 15:36:35 +0200

    amélioration

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-let films = ['Titanic', 'GOT', 'Harry Potter', 'Star Wards'];
-let spoils = ['Jack meurt', 'Les Starks meurent', 'C\'est mort je spoil pas', 'Dark Vador meurt']
-let film = '';
+let films = ['Titanic', 'Harry Potter', 'Star Wards', 'Tenet'];
+let series = ['GOT', 'Limitless', 'The Boys', 'The expanse'];
+let livres = ['Lotr', 'Cyrano De Bergerac', 'Comment paraître plus intelligent', 'Twilight'];
+let pick = '';
+
+let spoilsFilms = ['Jack meurt', 'C\'est mort je spoil pas', 'Dark Vador meurt', 'Robert meurt']
+let spoilsSeries = ['Les Starks meurent', 'Un gars dedans meurt', 'La nazi meurt', 'ça meurt beaucoup']
+let spoilsLivres = ['Gandalf meurt', 'Big Nose meurt', 'ça ne marche pas', 'Les méchants meurts']
+let spoil = '';
+
+
 let randomNum = 0;
-function random(films){
-    let random = Math.floor(Math.random() * films.length);
-    let randomFilm = films[random];
+function random(pick, spoilPick){
+    let random = Math.floor(Math.random() * pick.length);
+    let randomFilm = pick[random];
     let filmTitre = document.getElementById('filmTitre');
     let filmSpoil = document.getElementById('filmSpoil');
     filmSpoil.innerHTML='';
     filmTitre.innerHTML=randomFilm;
-    film=randomFilm;
+    pick=randomFilm;
+    spoil = spoilPick;
     randomNum = random;
 }
 
-function divulguacheRandom(film, spoils, randomNum){
-    let randomFilmSpoil = spoils[randomNum];
+function divulguacheRandom(pick, spoil, randomNum){
+    let randomFilmSpoil = spoil[randomNum];
     let filmSpoil = document.getElementById('filmSpoil');
     filmSpoil.innerHTML=randomFilmSpoil;
     console.log(film);
